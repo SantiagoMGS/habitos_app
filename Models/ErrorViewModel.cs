38e63c82f08aa9d683dc151aa99cdbeba94f5007
Santiago Martinez <smartinez9626@gmail.com>
2024-04-02 19:04:48 -0500

    Inicializacion de proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace habitos_app.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
